ca0942e91d164332c28a219926a291f748824930
Syzer209 <113340696+Syzer209@users.noreply.github.com>
2022-11-28 11:02:33 +0100

    ejerccio1

---
+++ Ejercicio1.py
@@ -0,0 +1,14 @@
+'Creo una función llamada:'
+def convert_rgb2gray(RGB):
+color = []
+for color in RGB:
+    R1 = input(int("Introduce un valor del 0-255 del color Rojo: "))
+    G1 = input(int("Introduce un valor del 0-255 del color Verde: "))
+    B1 = input(int("Introduce un valor del 0-255 del color Azul: "))
+    print((R1),(G1),(B1),color)
+
+
+
+def rgb2gray(Luminancia):
+Luninancia = ((R)+(G)+(B))/3
+
